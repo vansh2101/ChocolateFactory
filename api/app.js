dea1445b9d52288b8c4a8bb52002a0b64d75b7fa
Jai Nanda <85067748+Jai-Nanda@users.noreply.github.com>
2022-01-15 14:43:08 +0530

    feedbacks backend done

--- api/app.js
+++ api/app.js
@@ -3,14 +3,14 @@ const express = require('express')
 //routes
 const auth = require('./routes/auth')
 const user = require('./routes/user')
-
+const feedbacks = require('./routes/feedbacks')
 
 const app = express()
-app.listen(process.env.PORT || 8000)
-
 app.use('/auth', auth)
 app.use('/user', user)
+app.use('/feedbacks', feedbacks)
 
 app.get('/', (req, res) => {
-    res.send('Chocolate Factory API')
+    res.send('Chocolate Factory')
 })
+app.listen(process.env.PORT || 8000)
