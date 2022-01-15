1c579bb2bc0308d99fad4d135b70a746e44d54d3
Manan <mananman23@gmail.com>
2022-01-15 14:56:07 +0530

    tasks.js created

--- api/app.js
+++ api/app.js
@@ -3,13 +3,18 @@ const express = require('express')
 //routes
 const auth = require('./routes/auth')
 const user = require('./routes/user')
+const tasks = require('./routes/tasks')
 
 
 const app = express()
-app.listen(process.env.PORT || 8000)
+app.listen(8000, () => {
+    console.log('Server is running on port 8000')
+})
 
+app.use(express.json())
 app.use('/auth', auth)
 app.use('/user', user)
+app.use('/tasks', tasks)
 
 app.get('/', (req, res) => {
     res.send('Chocolate Factory API')
---
+++ api/routes/tasks.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const http = require('http');
+const sb = require('@supabase/supabase-js');
+
+//supabase
+const config = require('../supabase/config');
+supabase = sb.createClient(config.url, config.key)
+
+
+router.get("/", (req, res) => {
+  res.send("Supabase check")
+});
+
+router.post("/load", async (req, res) => {
+    const data = await supabase
+        .from('orders')
+        .select('status')
+        .match({status: 'pending'})
+        .select('id')
+        .then(data => {
+            // return {data};
+            if (data !== null) {
+                res.send({data});
+            } else {
+                res.send({error: 'No pending orders'});
+            }
+        });
+});
+
+// Update the status of the task using the task id
+router.post("/claim", async (req, res) => {
+    const data = await supabase
+        .from('orders')
+        .update({status: "inprogress", email: "test@gmail.com"})
+        .match({id: req.body.id})
+        .then(data => {
+            // return {data};
+            if (data !== null) {
+                res.send({data});
+            } else {
+                res.send({error: {error}});
+            }
+        });
+});
+
+router.post('/create', async (req, res) => {
+    const data = await supabase
+        .from('orders')
+        .insert({
+            name: req.body.name,
+            email: req.body.email,
+            flavour: req.body.flavour,
+            quantity: req.body.quantity,
+            status: "pending",
+            order_date: new Date()
+        })
+        .then(data => {
+            if (data !== null) {
+                res.send({data});
+            } else {
+                res.send({error: 'Error creating order'});
+            }
+        });
+});
+
+router.post('/complete', async (req, res) => {
+    const data = await supabase
+        .from('orders')
+        .update({status: "done"})
+        .match({id: req.body.id})
+        .then(data => {
+            return {data};
+        })
+});
+
+module.exports = router
