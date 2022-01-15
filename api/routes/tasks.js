f1042aa6bea7a942fbd5c97a9c46f8428dc9a62f
vansh sachdeva <vansh2101sachdeva@gmail.com>
2022-01-15 16:24:32 +0530

    fixed tasks.js

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -12,65 +12,24 @@ router.get("/", (req, res) => {
   res.send("Task manager")
 });
 
-router.post("/load", async (req, res) => {
-    const data = await supabase
-        .from('orders')
-        .select('status')
-        .match({status: 'pending'})
-        .select('id')
-        .then(data => {
-            // return {data};
-            if (data !== null) {
-                res.send({data});
-            } else {
-                res.send({error: 'No pending orders'});
-            }
-        });
-});
 
 // Update the status of the task using the task id
-router.post("/claim", async (req, res) => {
-    const data = await supabase
-        .from('orders')
-        .update({status: "inprogress", email: "test@gmail.com"})
+router.post("/claim", (req, res) => {
+    supabase.from('orders')
+        .update({status: "inprogress", employee: req.body.employee})
         .match({id: req.body.id})
         .then(data => {
-            // return {data};
-            if (data !== null) {
-                res.send({data});
-            } else {
-                res.send({error: {error}});
-            }
+            res.json(data)
         });
 });
 
-router.post('/create', async (req, res) => {
-    const data = await supabase
-        .from('orders')
-        .insert({
-            name: req.body.name,
-            email: req.body.email,
-            flavour: req.body.flavour,
-            quantity: req.body.quantity,
-            status: "pending",
-            order_date: new Date()
-        })
-        .then(data => {
-            if (data !== null) {
-                res.send({data});
-            } else {
-                res.send({error: 'Error creating order'});
-            }
-        });
-});
 
-router.post('/complete', async (req, res) => {
-    const data = await supabase
-        .from('orders')
-        .update({status: "done"})
+router.post('/complete', (req, res) => {
+    supabase.from('orders')
+        .update({status: "completed"})
         .match({id: req.body.id})
         .then(data => {
-            return {data};
+            res.json(data);
         })
 });
 
