10e1edb9ab33a597cdd8e957cf762658555b1e11
VANSH  SACHDEVA <68803315+vansh2101@users.noreply.github.com>
2022-01-16 10:27:56 +0530

    removed url and key

--- api/supabase/config.js
+++ api/supabase/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: 'https://qfxsiqquvygczdoavfqz.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MjEwNTg0OSwiZXhwIjoxOTU3NjgxODQ5fQ.Ibp6VALeGrGJfeyuuClkxZi_cOk0iABr_l9zXsTTZP0'
+    url: <supabase url>,
+    key: <supabase access key>
 }
