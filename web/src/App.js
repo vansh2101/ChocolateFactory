27048fac8f5375ec6a0f371746f1185c4a41ce27
VANSH  SACHDEVA <vansh2101sachdeva@gmail.com>
2022-01-17 23:15:06 +0530

    bug fix

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,17 @@
 import { BrowserRouter , Route,  Routes } from 'react-router-dom';
+import './styles/index.css';
+
+//screens
+import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
+
+
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
     </BrowserRouter>
   );
